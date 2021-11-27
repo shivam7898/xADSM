ba015ab222a38c815832fcce5586911e59f6ecde
shivam7898 <shivam7898@users.noreply.github.com>
2021-11-27 16:05:04 +0530

    +301 Done

---
+++ code/000Packages.R
@@ -0,0 +1,16 @@
+# # source(paste0(.z$RX, "000Packages", ".R")) ===================================================#
+
+if (!("conflicted" %in% (.packages()))) {
+  pkg <- character(0)
+  pkg <- c(pkg, "conflicted")
+  pkg <- c(pkg, "ggplot2", "tibble", "tidyr", "readr", "purrr", "dplyr", "stringr", "forcats")
+  pkg <- c(pkg, "data.table", "gifski", "nycflights13", "gapminder", "Lahman", "lubridate")
+  pkg <- c(pkg, "kableExtra", "readxl", "mice", "VIM", "moments", "qcc")
+  #lapply(pkg, FUN = library, character.only = TRUE)
+  lapply(pkg, FUN = function(x) {suppressMessages(library(x, character.only = TRUE))})
+#
+# #Precedence
+  invisible(suppressMessages(conflict_prefer("filter", "dplyr")))
+# #Cleanup
+  rm(pkg)
+} else print("Loaded!")
