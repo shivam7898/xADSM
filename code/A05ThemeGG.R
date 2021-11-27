c58a8918836fe5e16dc836453b422f2a762753fd
shivam7898 <shivam7898@users.noreply.github.com>
2021-11-28 01:51:21 +0530

    +Pie Chart

--- code/A05ThemeGG.R
+++ code/A05ThemeGG.R
@@ -1,12 +1,14 @@
 # # source(paste0(.z$RX, "A05ThemeGG", ".R")) ====================================================#
 if (!("ggplot2" %in% (.packages()))) library("ggplot2")
 
+## ---- A05-ThemeGG ----
 f_theme_gg <- function(base_size = 14) {
 # #Create a Default Theme 
   theme_bw(base_size = base_size) %+replace%
     theme(
       # #The whole figure
-      plot.title = element_text(size = rel(1), face = "bold", margin = margin(0,0,5,0), hjust = 0),
+      plot.title = element_text(size = rel(1), face = "bold", 
+	                            margin = margin(0,0,5,0), hjust = 0),
       # #Area where the graph is located
       panel.grid.minor = element_blank(),
       panel.border = element_blank(),
@@ -27,13 +29,30 @@ f_theme_gg <- function(base_size = 14) {
     )
 }
 
+## ---- A05-SetThemeNLists ----
 # #Change default ggplot2 theme 
 theme_set(f_theme_gg()) 
-
+#
 # #List of Specific sets. Note '+' is replaced by ','
 k_gglayer_bar <- list(
   scale_fill_viridis_d(),
   theme(panel.grid.major.x = element_blank(), axis.line = element_blank(),
-          panel.border = element_rect(colour = "black", fill=NA, size=1),
-          legend.position = "none", axis.title.y.right = element_blank())
+        panel.border = element_rect(colour = "black", fill=NA, size=1),
+        legend.position = 'none', axis.title.y.right = element_blank())
+)
+#
+# #Pie
+k_gglayer_pie <- list(
+  scale_fill_viridis_d(),
+  #theme_void(),
+  theme(#panel.background = element_rect(fill = "white", colour = "white"),
+		#plot.background = element_rect(fill = "white",colour = "white"),
+		axis.line = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        axis.title = element_blank(),
+        #panel.border = element_rect(colour = "black", fill=NA, size=1),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        legend.position = 'none')
 )
