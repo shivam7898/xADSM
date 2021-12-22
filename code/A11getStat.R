41a5d3fe94bfd258a612da8bc6def95e08ae20d6
shivam7898 <shivam7898@users.noreply.github.com>
2021-12-22 16:28:23 +0530

    WIP C10

--- code/A11getStat.R
+++ code/A11getStat.R
@@ -7,8 +7,10 @@ f_get2sSE <- function(s1, n1, s2, n2) {
   # #Same for both cases of known or unknown Standard Deviation 
   # # And Prints LaTex and R Equivalents for Notebook
   xx  <- round(sqrt({s1^2 /n1 + s2^2 /n2}), 4)
-  cat(paste0("- \\@ref(eq:se-2s-sd) ${\\sigma}_{({\\overline{x}}_1 - {\\overline{x}}_2)} = \\sqrt{\\frac{{", s1, "}^2}{", n1, "} + \\frac{{", s2, "}^2}{", n2, "}} = ", xx,"$\n"))
-  cat(paste0("- ", "`", "sqrt(", s1, "^2/", n1, " + ", s2, "^2/", n2, ")", "`", " \\textcolor{pink}{$\\#\\mathcal{R}$}", "\n"))
+  cat(paste0("- \\@ref(eq:se-2s-sd) ${\\sigma}_{({\\overline{x}}_1 - {\\overline{x}}_2)} = \\sqrt{\\frac{{", 
+  s1, "}^2}{", n1, "} + \\frac{{", s2, "}^2}{", n2, "}} = ", xx,"$\n"))
+  cat(paste0("- ", "`", "sqrt(", s1, "^2/", n1, " + ", s2, "^2/", n2, ")", 
+  "`", " \\textcolor{pink}{$\\#\\mathcal{R}$}", "\n"))
   return(xx)
 }
 
@@ -18,7 +20,10 @@ f_get2sDof <- function(s1, n1, s2, n2) {
   # # Return Degrees of Freedom for Two Sample t-test for unknown Standard Deviation
   # Value rounded down to provide a larger t-value and a more conservative interval estimate
   xx  <- floor({s1^2 / n1 + s2^2 / n2 }^2 / {{s1^2 / n1}^2/{n1-1} + {s2^2 / n2}^2/{n2-1}})
-  cat(paste0("- ", "`", "floor({", s1,"^2 / ", n1, " + ", s2, "^2 / ", n2, " }^2 / {{", s1, "^2 / ", n1, "}^2/{", n1, "-1} + {", s2, "^2 / ", n2, "}^2/{", n2, "-1}})", "`", " \\textcolor{pink}{$\\#\\mathcal{R}$}", "\n"))
+  cat(paste0("- ", "`", "floor({", s1,"^2 / ", n1, " + ", s2, "^2 / ", n2, " }^2 / {{", 
+  s1, "^2 / ", n1, "}^2/{", n1, "-1} + {", s2, "^2 / ", n2, "}^2/{", n2, "-1}})", 
+  "`", " \\textcolor{pink}{$\\#\\mathcal{R}$}", "\n"))
+  cat(paste0("DOF = ", xx, "\n"))
   return(xx)
 }
 
@@ -27,9 +32,14 @@ f_getSeProp <- function(p1, n1, p2, n2) {
   # f_getSeProp(0.5, 11, 0.25, 22)
   # # Return Standard Error SE for Proportions
   # Value rounded down to provide a larger t-value and a more conservative interval estimate
-  xx  <- sqrt(p1 * (1 - p1) / n1 + p2 * (1 - p2) / n2)
-  cat(paste0("- ", "`", "sqrt(", p1, " * (1 - ", p1, ") / ", n1, " + ", p2, " * (1 - ", p2, ") / ", n2, ")", "`", " \\textcolor{pink}{$\\#\\mathcal{R}$}", "\n"))
-  cat(paste0("- ", "`", "{", n1, " * ", p1, " + ", n2, " * ", p2, "} / {", n1, " + ", n2, "}", "`", " \\textcolor{pink}{$\\#\\mathcal{R}$}", "\n"))
+  xx <- sqrt(p1 * (1 - p1) / n1 + p2 * (1 - p2) / n2)
+  yy <- {n1 * p1 + n2 * p2} / {n1 + n2}
+  cat(paste0("- ", "`", "sqrt(", p1, " * (1 - ", p1, ") / ", n1, " + ", p2, " * (1 - ", p2, 
+  ") / ", n2, ")", "`", " \\textcolor{pink}{$\\#\\mathcal{R}$}", "\n"))
+  cat(paste0("SE = ", xx, "\n"))
+  cat(paste0("- ", "`", "{", n1, " * ", p1, " + ", n2, " * ", p2, "} / {", n1, " + ", n2, "}", 
+  "`", " \\textcolor{pink}{$\\#\\mathcal{R}$}", "\n"))
+  cat(paste0("p = ", yy, "\n"))
   return(xx)
 }
 
