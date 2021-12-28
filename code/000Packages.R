26d8f215eaf15981c8e54c46b59b299371a34c01
shivam7898 <shivam7898@users.noreply.github.com>
2021-12-29 02:16:30 +0530

    WIP B15

--- code/000Packages.R
+++ code/000Packages.R
@@ -4,7 +4,7 @@ if (!("conflicted" %in% (.packages()))) {
   pkg <- character(0)
   pkg <- c(pkg, "conflicted")
   pkg <- c(pkg, "ggplot2", "tibble", "tidyr", "readr", "purrr", "dplyr", "stringr", "forcats")
-  pkg <- c(pkg, "kableExtra", "readxl", "latex2exp", "e1071", "lubridate", "car")
+  pkg <- c(pkg, "kableExtra", "readxl", "latex2exp", "e1071", "lubridate", "car", "zoo")
   pkg <- c(pkg, "data.table", "gifski", "nycflights13", "gapminder", "Lahman")
   pkg <- c(pkg, "mice", "VIM", "qcc", "ggpmisc", "microbenchmark")
   pkg <- c(pkg, "compiler", "viridisLite", "scales", "psych")
@@ -17,6 +17,12 @@ if (!("conflicted" %in% (.packages()))) {
   invisible(suppressMessages(conflict_prefer("annotate", "ggplot2")))
 # # dplyr::lag | stats::lag
   invisible(suppressMessages(conflict_prefer("lag", "dplyr"))) 
+# # lubridate::week | data.table::week
+  invisible(suppressMessages(conflict_prefer("week", "lubridate"))) 
+# # lubridate::year | data.table::year
+  invisible(suppressMessages(conflict_prefer("year", "lubridate")))  
+# # lubridate::month | data.table::month
+  invisible(suppressMessages(conflict_prefer("month", "lubridate")))  
 # #Cleanup
   rm(pkg)
 } else print("Packages not loaded again.")
