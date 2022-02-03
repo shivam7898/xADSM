c573a0f2b0ff5c098d4036dcd0d504378f7556c9
shivam7898 <shivam7898@users.noreply.github.com>
2022-02-03 19:39:49 +0530

    Appended B24 with Visualisations & Reverted latex2exp Package to 0.5.0 see 212

--- code/000Packages.R
+++ code/000Packages.R
@@ -8,8 +8,9 @@ if (!("conflicted" %in% (.packages()))) {
   pkg <- c(pkg, "data.table", "gifski", "nycflights13", "gapminder", "Lahman", "nortest")
   pkg <- c(pkg, "mice", "VIM", "qcc", "ggpmisc", "microbenchmark", "GGally", "corrplot")
   pkg <- c(pkg, "compiler", "viridisLite", "scales", "psych", "fastDummies", "RColorBrewer")
-  pkg <- c(pkg, "caret", "glmnet", "mlbench", "stringi", "rfm", "factoextra", "cluster")
+  pkg <- c(pkg, "caret", "glmnet", "mlbench", "stringi", "factoextra", "cluster")
   pkg <- c(pkg, "arules", "arulesViz", "ggrepel")
+  pkg <- c(pkg, "rfm") #Threw error on Feb-03. Keep it separate for now.
   #lapply(pkg, FUN = library, character.only = TRUE)
   lapply(pkg, FUN = function(x) {suppressMessages(library(x, character.only = TRUE))})
 #
